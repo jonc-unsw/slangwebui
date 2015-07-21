c307da98423abeb606e67c7e1358ad60808199ce
Jon Chronopoulos <z3323488@student.unsw.edu.au>
2015-07-21 14:30:25 +1000

    Format the charts a bit

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import { BarChart, PieChart, LineChart } from 'react-d3';
-/*import rd3 from 'react-d3';
-var BarChart = rd3.BarChart;
-var PieChart = rd3.PieChart;*/
 
 class MyChart extends React.Component {
   render(){
     var pieData = [
-      {label: "Control Flow Edges", value: 20.0}, 
-      {label: "Data Dependence Edges", value: 55.0}
+      {label: "Control Flow", value: 20.0}, 
+      {label: "Data Dependence", value: 55.0}
     ];
     
     var barData = [
-      {label: "Control Flow Edges", value: 20.0}, 
-      {label: "Data Dependence Edges", value: 55.0}
+      {label: "Control Flow", value: 20.0}, 
+      {label: "Data Dependence", value: 55.0}
     ];
 
     return (
-      <div>
-        <PieChart className="fff" data={pieData} width={450} height={400} radius={110}
-          innerRadius={20} sectorBorderColor="white" title="Pie Chart" />
-        <BarChart data={barData} width={500} height={300} title="Bar Chart"
-          yAxisLabel="Label" xAxisLabel="Value"/>
+      <div style={{'display': 'flex'}}>
+        <div style={{'flex': 1}} ><PieChart data={pieData} width={450} height={400} radius={110}
+          innerRadius={20} sectorBorderColor="white" title="Edges" /></div>
+        <div style={{'flex': 1}}><BarChart data={barData} width={500} height={300} title="Edges"
+          yAxisLabel="Percentage" xAxisLabel="Edges"/></div>
       </div>
     )
   }
