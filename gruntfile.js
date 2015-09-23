0694264d46e7a479ced475d49e76c92e6fb2fd60
Jon Chronopoulos <z3323488@student.unsw.edu.au>
2015-09-23 11:54:56 +1000

    lint gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -5,31 +5,29 @@ module.exports = function(grunt){
     browserify: {
       dev: {
         options: {
-            plugin: [
-              [ "licensify"],
-            ],
-            debug: true,
-            transform: [["babelify", { "stage": 0 }]],
-            browserifyOptions: {
-              extensions: ['.jsx']
-            }
+          plugin: [
+            [ "licensify"]
+          ],
+          debug: true,
+          transform: [["babelify", { "stage": 0 }]],
+          browserifyOptions: {
+            extensions: [".jsx"]
+          }
         },
         files: {
-            'dist/app.js': 'src/**/*.jsx',
+          "dist/app.js": "src/**/*.jsx"
         }
       }
     },
     uglify: {
       my_target: {
         files: {
-          'dist/static.js': [
-              './node_modules/jquery/dist/jquery.min.js',
-              './jquery.qtip.js',
-              './node_modules/cytoscape/dist/cytoscape.js',
-              './node_modules/cytoscape-qtip/cytoscape-qtip.js',
-              './node_modules/bootstrap/dist/js/bootstrap.min.js',
-              './node_modules/babel-core/browser-polyfill.min.js',
-              './node_modules/cytoscape/lib/dagre.js'
+          "dist/static.js": [
+            "./node_modules/jquery/dist/jquery.min.js",
+            "./jquery.qtip.js",
+            "./node_modules/bootstrap/dist/js/bootstrap.min.js",
+            "./node_modules/babel-core/browser-polyfill.min.js",
+            "./node_modules/cytoscape/lib/dagre.js"
           ]
         }
       }
@@ -41,18 +39,18 @@ module.exports = function(grunt){
       },
       target: {
         files: {
-          'dist/app.css': [
-            './node_modules/bootstrap/dist/css/bootstrap.css', 
-            './node_modules/codemirror/lib/codemirror.css',
-            './node_modules/react-treeview/react-treeview.css'
+          "dist/app.css": [
+            "./node_modules/bootstrap/dist/css/bootstrap.css", 
+            "./node_modules/codemirror/lib/codemirror.css",
+            "./node_modules/react-treeview/react-treeview.css"
           ]
         }
       }
     },
     watch: {
       browserify: {
-        files: ['src/**/*.jsx', 'src/**/*.js'],
-        tasks: ['browserify:dev']
+        files: ["src/**/*.jsx", "src/**/*.js"],
+        tasks: ["browserify:dev"]
       },
       options: {
         nospawn: true
@@ -61,20 +59,20 @@ module.exports = function(grunt){
     compress: {
       main: {
         options: {
-          archive: 'demo.tgz',
+          archive: "demo.tgz"
         },
         files: [
-          {src: ['dist/**','projects/**','index.html', 'server.py'], dest: 'demo/', filter: 'isFile', expand: true}, // includes files in path and its subdirs
+          {src: ["dist/**","projects/**","index.html", "server.py"], dest: "demo/", filter: "isFile", expand: true} // includes files in path and its subdirs
         ]
       }
     }    
-  })
+  });
 
-grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.loadNpmTasks('grunt-browserify');
-grunt.loadNpmTasks('grunt-contrib-cssmin');
-grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.loadNpmTasks('grunt-contrib-compress');
-grunt.registerTask('default', ['watch']);
-//grunt.registerTask('build', ['env:build', 'browserify:build']);
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-browserify");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-compress");
+  grunt.registerTask("default", ["watch"]);
+  //grunt.registerTask('build', ['env:build', 'browserify:build']);
 };
