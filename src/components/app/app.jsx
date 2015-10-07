eaea0a4417faee64fae32fac9fa0cf9bb343613f
Jon Chronopoulos <z3323488@student.unsw.edu.au>
2015-10-07 13:54:25 +1100

    Start to use the flux pattern correctly...

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,7 @@
 "use strict";
 import React from "react";
 import { Accordion } from "react-bootstrap";
+import connectToStores from "../../../node_modules/alt/utils/connectToStores";
 
 import $ from "jquery";
 
@@ -13,7 +14,11 @@ import { Graph } from "./graph.jsx";
 import { BasicBlocks } from "./bb.jsx";
 import { DataDependenceBlocks } from "./ddb.jsx";
 
-class App extends React.Component {
+import { ProjectActions } from "../../actions/Actions.js";
+import { ProjectStore } from "../../stores/Store.js";
+
+
+@connectToStores class App extends React.Component {
   constructor( props ) {
     super( props );
     this.state = {
@@ -23,9 +28,18 @@ class App extends React.Component {
     this.PROJECTS_DIR = `/projects/${this.props.params.id}`;
   }
 
+  static getStores() {
+    return [ ProjectStore ];
+  }
+
+  static getPropsFromStores() {
+    return ProjectStore.getState();
+  }
+
   componentDidMount() {
     $.get( `${this.PROJECTS_DIR}/project.json`, ( result ) => {
       this.setState( { project: result } );
+      ProjectActions.loadProject( { root: this.PROJECTS_DIR, url: result.source } );
     } );
   }
 
@@ -52,7 +66,7 @@ class App extends React.Component {
         <Layout type="columns" >
           <Fixed className="sidebar" >
             <Accordion>
-              <Project root={this.PROJECTS_DIR} url={this.state.project.source} name={this.state.project.name} />
+              <Project name={this.state.project.name} source={this.props.source} />
               <SourceCode prefix={this.PROJECTS_DIR} file="fib/main.c" />
               <Features root={this.PROJECTS_DIR} url={this.state.project.features} />
             </Accordion>
--- src/components/app/project.jsx
+++ src/components/app/project.jsx
@@ -1,23 +1,12 @@
 "use strict";
 import React from "react";
-import connectToStores from "../../../node_modules/alt/utils/connectToStores";
-import { ProjectStore } from "../../stores/Store.js";
-import { ProjectActions, SourceCodeActions } from "../../actions/Actions.js";
+import { SourceCodeActions } from "../../actions/Actions.js";
 import { IndependentPanel } from "./independentpanel.jsx";
 import TreeView from "react-treeview";
 
-@connectToStores class Project extends React.Component {
+class Project extends React.Component {
   constructor( props ) {
     super( props );
-    ProjectActions.loadProject( { root: this.props.root, url: this.props.url } );
-  }
-
-  static getStores() {
-    return [ ProjectStore ];
-  }
-
-  static getPropsFromStores() {
-    return ProjectStore.getState();
   }
 
   handleClick = ( source ) => {
@@ -47,23 +36,15 @@ import TreeView from "react-treeview";
 
   render() {
 
-    if( this.props.source ) {
+    let files = this.props.source && this.generatePath( this.props.source, 0 );
 
-      let files = this.generatePath( this.props.source, 0 );
-
-      return (
-        <IndependentPanel {...this.props} header="Project" expanded={this.props.expanded} >
-          <TreeView nodeLabel={this.props.name} defaultCollapsed={false} >
-            {files}
-          </TreeView>
-        </IndependentPanel>
-      );
-    }
-    else {
-      return (<div>Loading</div>);
-    }
-
-
+    return (
+      <IndependentPanel {...this.props} header="Project" expanded={this.props.expanded} >
+        <TreeView nodeLabel={this.props.name} defaultCollapsed={false} >
+          {files}
+        </TreeView>
+      </IndependentPanel>
+    );
   }
 }
 
